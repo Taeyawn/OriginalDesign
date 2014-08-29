463112aff5e58a3fc9d2bb683d2fc3a8e7e6b92c
Taeyawn <MatthewASDFGHJKL@gmail.com>
2014-08-29 09:59:36 -0700

    Matthew

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,9 +1,31 @@
 void setup()
 {
-
+	size(600,600);
 }
 void draw()
 {
-  ellipse(50,50,50,50);
+  ears();
+  face();
 }
 
+void face()
+{
+	fill(255);
+	ellipse(300,300,300,300);
+	fill(0);
+	ellipse(240,260,80,50);
+	ellipse(360,260,80,50);
+	ellipse(300,320,45,25);
+	fill(255);
+	ellipse(250,260,30,30);
+	ellipse(370,260,30,30);
+	strokeWeight(8);
+	line(300,320,300,350);
+	line(280,350,320,350);
+}
+void ears()
+{
+	fill(5);
+    ellipse(210,200,90,90);
+    ellipse(390,200,90,90);
+}
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -16,13 +16,35 @@ public class OriginalDesign extends PApplet {
 
 public void setup()
 {
-
+	size(600,600);
 }
 public void draw()
 {
-  ellipse(50,50,50,50);
+  ears();
+  face();
 }
 
+public void face()
+{
+	fill(255);
+	ellipse(300,300,300,300);
+	fill(0);
+	ellipse(240,260,80,50);
+	ellipse(360,260,80,50);
+	ellipse(300,320,45,25);
+	fill(255);
+	ellipse(250,260,30,30);
+	ellipse(370,260,30,30);
+	strokeWeight(8);
+	line(300,320,300,350);
+	line(280,350,320,350);
+}
+public void ears()
+{
+	fill(5);
+    ellipse(210,200,90,90);
+    ellipse(390,200,90,90);
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
